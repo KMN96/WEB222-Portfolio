e6e493e714838a8857e93c6db0105371169fbb00
KMN96 <65213234+KMN96@users.noreply.github.com>
2021-12-12 06:34:38 -0500

    Add files via upload

---
+++ assets/js/hidden.js
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+
+function yesnoCheck() {
+		if(document.getElementById('Hiring').checked) {
+			document.getElementById('ifHiring').style.display = 'block';
+		}
+		else document.getElementById('ifHiring').style.display = 'none';
+	}
+
+</script>
